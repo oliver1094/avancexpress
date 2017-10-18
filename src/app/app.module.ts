d7e7268fa2e27efe6351dd3e2bb3d05f0d3948a0
JosafatPadilla <j.padilla.chi@gmail.com>
2017-10-18 16:49:32 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatSliderModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import {D3SliderDirective} from 'ng-d3-slider/d3-slider.directive';
+import { MyDatePickerModule } from 'mydatepicker';
+
+// rutas
+import { APP_ROUTING } from './app.routes';
+import { NavbarComponent } from './components/landing-page/navbar/navbar.component';
+import { FooterComponent } from './components/landing-page/footer/footer.component';
+import { MainSlideComponent } from './components/landing-page/main-slide/main-slide.component';
+import { SectionContactComponent } from './components/landing-page/section-contact/section-contact.component';
+import { SectionBenefitsComponent } from './components/landing-page/section-benefits/section-benefits.component';
+import { SectionRequestComponent } from './components/landing-page/section-request/section-request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainSlideComponent,
+    SectionContactComponent,
+    SectionBenefitsComponent,
+    SectionRequestComponent,
+    D3SliderDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    MatSliderModule,
+    MyDatePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
